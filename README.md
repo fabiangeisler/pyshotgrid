147e8f2bf26550c82e5547f646f90930e91e70d4
Fabian Geisler <fabian.geisler@automatik-vfx.com>
2022-06-08 23:36:32 +0200

    Add a lot of docstrings

--- README.md
+++ README.md
@@ -14,29 +14,33 @@ If you want to query a field from that instance you can do so via dictionary sty
 
 ```python
 >>> import shotgun_api3
->>> import pyshotgrid
+>>> import pyshotgrid as pysg
 >>> sg = shotgun_api3.Shotgun(
 ... base_url = 'https://example.shotgunstudio.com',
 ... script_name = 'Some User',
 ... api_key = '$ome_password')
->>> sg_project = pyshotgrid.ShotGridEntity(sg, "Project", 1)
+>>> sg_project = pysg.convert(sg, "Project", 1)
 >>> sg_project["code"]
 "Test Project"
 ```
 
-A ShotGridEntity instance represents exactly one entity and
-any operation on it is reflected to ShotGrid.
+A ShotGridEntity instance represents exactly one entity and any operation on it is reflected to ShotGrid.
 So for example you can :
 
 * Get fields from ShotGrid
   ```python
   print(sg_project["code"])  # "foobar"
-  print(sg_project.get(["code", "tank_name"]))  # {"code":"foobar", "tank_name": "fb"}
+  print(sg_project.get(["code", "tank_name"]))  # {"code": "foobar", "tank_name": "fb"}
+  ```
+* Values are automatically converted to `pyshotgrid` objects which makes it
+  possible to chain queries together.
+  ```python
+  print(sg_playlist["versions"][0]["code"])  # Name of the first Version in the Playlist.
   ```
 * Update fields in ShotGrid
   ```python
   sg_project["code"] = "foobar"
-  sg_project.set({"code":"foobar", "tank_name": "fb"})
+  sg_project.set({"code": "foobar", "tank_name": "fb"})
   ```
 * Upload/Download to/from a field
   ```python
@@ -47,7 +51,7 @@ So for example you can :
   ```python
   print(sg_project.url)  # https://example.shotgunstudio.com/detail/Project/1
   ```
-* Convert it to a regular dict which can be used in the regular shotgun_api3
+* Convert it to a regular dict which can be used in the regular `shotgun_api3`.
   ```python
   sg_project.to_dict()  # {"type": "Project", "id": 1}
   ```
@@ -56,13 +60,35 @@ So for example you can :
   for field, value in sg_project.all_fields().items():
        print(field, value)
   ```
-The rules is: All fields of an entity need to be accessed via dict notation (eg. `sg_project['code']`)
+The rules is: All fields of an entity are accessed via dict notation (eg. `sg_project['code']`)
               and all "special" functionality is accessed via instance methods (eg. `sg_version.download('sg_movie', '/path/to/somewhere')`).
 
+## How it works
 
-## Is it faster than `shotgun_api3`?
+Behind the scenes we have one `ShotGridEntity` class which is the base class for all
+other classes. It represents a single entity of a ShotGrid site and only
+stores 3 values:
+ - The shotgun_api3.Shotgun instance that is used to get and set all the information from.
+ - The ShotGrid entity type
+ - The ShotGrid entity ID
+
+All functionality of the `ShotGridEntity` class is based on these three values.
+There are some "special" classes that inherit from `ShotGridEntity` to add some
+common functionality to common entities in ShotGrid. For example the `SGProject`
+class adds a `shots()` function which gives you a list of shots from that project.
+To ensure that you are always using the right class you should use the `pyshotgrid.convert()`
+function which will return the right instance for you.
+
+## FAQ
+
+### Is it faster than `shotgun_api3`?
 No, and since it is build on top of `shotgun_api3`, it never will be.
 `pyshotgrid` is syntactic sugar that hopefully enables you to develop better and faster. :)
+
+### Is `pyshotgrid` replacing `shotgun_api3`?
+No, quite the opposite. It is meant to be used in conjunction with `shotgun_api3` and
+improve handling and writing code with it. Its main goal is to make it easier to write
+code for common scenarios and leave the special cases for the `shotgun_api3`.
 
 ## Credits
 
--- src/pyshotgrid/base.py
+++ src/pyshotgrid/base.py
@@ -1,14 +1,30 @@
+import os.path
+
 import shotgun_api3
 
 
 def convert(sg, *args, **kwargs):
+    """
+    Convert a given ShotGrid entity to an instance of a pyshotgrid class.
+    The function can be used in 3 ways which all do the same thing:
+
+            convert(sg, {'type': 'Project', 'id': 1})
+            convert(sg, 'Project', 1)
+            convert(sg, entity_type='Project', entity_id=1)
+
+    :param shotgun_api3.Shotgun sg: A fully initialized Shotgun instance.
+    :return: The pyshotgrid object or None if it could not be converted.
+    :rtype: ShotGridEntity|None
+    """
     entity_type = None
     entity_id = None
     if args:
         if isinstance(args[0], dict):
+            # convert(sg, {'type': 'Project', 'id': 1})
             entity_type = args[0]['type']
             entity_id = args[0]['id']
         elif len(args) == 2 and isinstance(args[0], str) and isinstance(args[1], int):
+            # convert(sg, 'Project', 1)
             entity_type = args[0]
             entity_id = args[1]
     elif kwargs:
@@ -29,11 +45,11 @@ def convert(sg, *args, **kwargs):
 
 class ShotGridEntity(object):
     """
-    This class represents a single entity in ShotGrid.
+    An instance of this class represents a single entity in ShotGrid.
 
-    :ivar shotgun_api3.Shotgun sg:
-    :ivar str entity_type:
-    :ivar int entity_id:
+    :ivar shotgun_api3.Shotgun sg: A fully initialized instance of shotgun_api3.Shotgun.
+    :ivar str entity_type: The ShotGrid type of the entity.
+    :ivar int entity_id: The ID of the ShotGrid entity.
     """
 
     def __init__(self, sg, entity_type, entity_id):
@@ -47,14 +63,26 @@ class ShotGridEntity(object):
 
     @property
     def id(self):
+        """
+        :return: The ID of the ShotGrid entity.
+        :rtype: int
+        """
         return self._id
 
     @property
     def type(self):
+        """
+        :return: The type of the ShotGrid entity.
+        :rtype: str
+        """
         return self._type
 
     @property
     def sg(self):
+        """
+        :return: The Shotgun instance that the entity belongs to.
+        :rtype: shotgun_api3.Shotgun
+        """
         return self._sg
 
     @property
@@ -69,6 +97,13 @@ class ShotGridEntity(object):
         return '{}/detail/{}/{}'.format(self.sg.base_url, self._type, self._id)
 
     def __getitem__(self, field):
+        """
+        Enabling dict notation to query fields of the entity from ShotGrid.
+        Values will be automatically converted to pyshotgrid objects.
+
+        :param field:
+        :return:
+        """
         value = self.sg.find_one(self._type,
                                  [['id', 'is', self._id]],
                                  [field]).get(field)
@@ -85,13 +120,17 @@ class ShotGridEntity(object):
                        self._id,
                        data={key: value})
 
-    def all_fields(self, project_entity=None):
+    def all_fields(self, project_entity=None, raw_values=False):
         """
+        :param dict[str,Any]|ShotGridEntity project_entity: A project entity to filter by.
+        :param bool raw_values: Whether to convert entities to pysg objects or not.
+        :return: All fields and values from this entity in a dict. If a project entity is given
+                 only fields that are visible to the project are returned.
+        :rtype: dict[str,Any]
+        """
+        if isinstance(project_entity, ShotGridEntity):
+            project_entity = project_entity.to_dict()
 
-        :param dict[str|Any] project_entity:
-        :return:
-        """
-        # TODO convert project_entity
         sg_entity_fields = self.sg.schema_field_read(self._type, project_entity=project_entity)
         fields = [field
                   for field, schema in sg_entity_fields.items()
@@ -99,8 +138,11 @@ class ShotGridEntity(object):
         all_fields = self.sg.find_one(self._type,
                                       [['id', 'is', self._id]],
                                       fields)
-        # TODO convert entities to pyshotgrid
-        return all_fields
+
+        if raw_values:
+            return all_fields
+        else:
+            return self._convert_fields_to_pysg(all_fields)
 
     def to_dict(self):
         # noinspection PyUnresolvedReferences
@@ -120,7 +162,7 @@ class ShotGridEntity(object):
                   Useful when you want to collect field changes and set them in one go.
         :rtype: dict
         """
-        # TODO convert pyshotgrid values to dicts
+        data = self._convert_fields_to_dicts(data)
         return {"request_type": "update",
                 "entity_type": self._type,
                 "entity_id": self._id,
@@ -191,32 +233,120 @@ class ShotGridEntity(object):
         """
         return self.sg.schema_field_read(self._type, field)[field]
 
-    def upload(self, field, path, display_name=None, tag_list=None):
+    def upload(self, field, path, display_name=None):
         """
-        Upload a file to a field
+        Upload a file to a field.
+
+        :param str field: The field to upload to.
+        :param str path: The path to the file to upload.
+        :param str display_name: The display name of the file in ShotGrid.
+        :return: The Attachment entity that was created for the uploaded file.
+        :rtype: ShotGridEntity
         """
-        # TODO convert tag list pysg objects
-        return self.sg.upload(entity_type=self._type,
-                              entity_id=self._id,
-                              path=path,
-                              field_name=field,
-                              display_name=display_name,
-                              tag_list=tag_list)
+        sg_attachment_id = self.sg.upload(entity_type=self._type,
+                                          entity_id=self._id,
+                                          path=path,
+                                          field_name=field,
+                                          display_name=display_name)
+        return convert(self._sg, "Attachment", sg_attachment_id)
 
     def download(self, field, path):
         """
         Download a file from a field.
+
+        :param str field: The field to download from.
+        :param str path: The path to download to.
+        :return:
         """
-        return self.sg.download_attachment(attachment=self[field], file_path=path)
+        # TODO What if the field is empty?
+        sg_attachment = self._sg.find_one(self._type,
+                                          [["id", "is", self._id]],
+                                          [field])[field]
+        # if we can split of a file extension from the given path we assume that the path is the
+        # full path with file name to download to. In the other case we assume that the path is
+        # the directory to download to and attach the attachment name as the file name to the
+        # directory path.
+        _, ext = os.path.splitext(path)
+        if ext:
+            local_file_path = os.path.join(path, sg_attachment["name"])
+        else:
+            local_file_path = path
+
+        return self.sg.download_attachment(attachment=sg_attachment,
+                                           file_path=local_file_path)
+
+    def _convert_fields_to_pysg(self, fields):
+        """
+        Convert all the values from a fields dict to pysg objects where possible.
+
+        :param dict[str,Any] fields: A fields dict as returned from a shotgun_api3.Shotgun.find()
+                                     call for example.
+        :return: The same dict with all values converted to pysg objects where possible.
+        :rtype: dict[str,Any]
+        """
+        result = {}
+        for field, value in fields.items():
+
+            if isinstance(value, list):
+                result[field] = [convert(self._sg, entity) for entity in value]
+            elif isinstance(value, dict) and 'type' in value and 'id' in value:
+                result[field] = convert(self._sg, value)
+            else:
+                result[field] = value
+
+        return result
+
+    @staticmethod
+    def _convert_fields_to_dicts(fields):
+        """
+        Convert all the values from a fields dict to simple dictionaries. The counterpart function
+        to `func:_convert_fields_to_pysg`.
+
+        :param dict[str,Any] fields: A fields dict as returned from a shotgun_api3.Shotgun.find()
+                                     call for example.
+        :return: The same dict with all pysg objects converted to dictionaries.
+        :rtype: dict[str,Any]
+        """
+        result = {}
+        for field, value in fields.items():
+
+            if isinstance(value, list):
+                tmp = []
+                for entity in value:
+                    if isinstance(entity, ShotGridEntity):
+                        tmp.append(entity.to_dict())
+                    else:
+                        tmp.append(entity)
+                result[field] = tmp
+            elif isinstance(value, ShotGridEntity):
+                result[field] = value.to_dict()
+            else:
+                result[field] = value
+
+        return result
 
 
 class SGSite(object):
+    """
+    An instance of this class represents the ShotGrid site as a whole.
+
+    :ivar shotgun_api3.Shotgun sg: A fully initialized instance of shotgun_api3.Shotgun.
+    """
 
     @classmethod
     def from_credentials(cls, base_url, script_name, api_key):
-        return cls(shotgun_api3.Shotgun(base_url=base_url,
-                                        script_name=script_name,
-                                        api_key=api_key))
+        """
+        Initialize the site with some basic ShotGrid API credentials.
+
+        :param str base_url: The URL to the ShotGrid site.
+        :param str script_name: The name of the script.
+        :param str api_key: The API key.
+        :return: An instance of this class.
+        :rtype: SGSite
+        """
+        return cls(sg=shotgun_api3.Shotgun(base_url=base_url,
+                                           script_name=script_name,
+                                           api_key=api_key))
 
     def __init__(self, sg):
         self.sg = sg
@@ -247,28 +377,43 @@ class SGSite(object):
                                if (sg_project['tank_name'] in names_or_ids or
                                    sg_project['name'] in names_or_ids)]
 
-        return [SGProject(self.sg, sg_project['id']) for sg_project in sg_projects]
+        return [convert(self.sg, sg_project) for sg_project in sg_projects]
 
 
 class SGProject(ShotGridEntity):
+    """
+    An instance of this class represents a single Project entity in ShotGrid.
+
+    :ivar shotgun_api3.Shotgun sg: A fully initialized instance of shotgun_api3.Shotgun.
+    :ivar int project_id: The ID of the project entity.
+    """
 
     def __init__(self, sg, project_id):
         super(SGProject, self).__init__(sg, entity_type='Project', entity_id=project_id)
 
     def shots(self):
+        """
+        :return: All the shots from this project.
+        :rtype: list[SGShot]
+        """
         sg_shots = self.sg.find('Shot', [['project', 'is', {'type': 'Project',
                                                             'id': self.id}]])
-        return [SGShot(self.sg, sg_shot['id']) for sg_shot in sg_shots]
+        return [convert(self._sg, sg_shot) for sg_shot in sg_shots]
 
 
 class SGShot(ShotGridEntity):
+    """
+    An instance of this class represents a single Shot entity in ShotGrid.
+
+    :ivar shotgun_api3.Shotgun sg: A fully initialized instance of shotgun_api3.Shotgun.
+    :ivar int shot_id: The ID of the shot entity.
+    """
 
     def __init__(self, sg, shot_id):
         super(SGShot, self).__init__(sg, entity_type='Shot', entity_id=shot_id)
 
     def publishes(self, pub_types=None, latest=False, additional_sg_filter=None):
         """
-
         :param str|list[str]|None pub_types: The names of the Publish File Types to return.
         :param bool latest: Whether to get the "latest" publishes or not. This uses the
                             same logic as the tk-multi-loader2 app which is as follows:
@@ -277,9 +422,10 @@ class SGShot(ShotGridEntity):
                              - if there are publishes with the same "name" and "version_number" the
                                newest one wins.
         :param additional_sg_filter:
-        :return:
+        :return: All published files from this shot.
+        :rtype: list[SGPublishedFile]
         """
-        base_filter = [['entity', 'is', {'type': 'Shot', 'id': self.id}]]
+        base_filter = [['entity', 'is', self.to_dict()]]
         if pub_types is not None:
             if isinstance(pub_types, list):
                 pub_types_filter = {"filter_operator": "any", "filters": []}
@@ -317,10 +463,16 @@ class SGShot(ShotGridEntity):
 
             sg_publishes = result
 
-        return [SGPublishedFile(self.sg, sg_publish['id']) for sg_publish in sg_publishes]
+        return [convert(self._sg, sg_publish) for sg_publish in sg_publishes]
 
 
 class SGPublishedFile(ShotGridEntity):
+    """
+    An instance of this class represents a single PublishedFile entity in ShotGrid.
+
+    :ivar shotgun_api3.Shotgun sg: A fully initialized instance of shotgun_api3.Shotgun.
+    :ivar int published_file_id: The ID of the PublishedFile entity.
+    """
 
     def __init__(self, sg, published_file_id):
         super(SGPublishedFile, self).__init__(sg, entity_type='PublishedFile',
