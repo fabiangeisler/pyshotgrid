d62e4fb5572096dc07d8248fd1484d0711ca4d36
Fabian Geisler <fabian.geisler@automatik-vfx.com>
2022-07-01 10:59:23 +0200

    Fix tox setup to run tests on all environments

--- src/pyshotgrid/__init__.py
+++ src/pyshotgrid/__init__.py
@@ -1,17 +1,17 @@
 """Main module."""
 
-from .core import *
+from .core import convert, register_plugin, register_fallback_pysg_class  # noqa: F401
 from .sg_entity import SGEntity
-from .sg_site import SGSite
-from .sg_default_entities import *
+from .sg_site import SGSite  # noqa: F401
+from . import sg_default_entities as sde
 
 #: The pyshotgrid version number as string
 VERSION = '0.1.0'
 
 register_fallback_pysg_class(SGEntity)
 # Register default pysg plugins
-register_plugin('Project', SGProject)
-register_plugin('Shot', SGShot)
-register_plugin('Task', SGTask)
-register_plugin('PublishedFile', SGPublishedFile)
-register_plugin('HumanUser', SGHumanUser)
+register_plugin('Project', sde.SGProject)
+register_plugin('Shot', sde.SGShot)
+register_plugin('Task', sde.SGTask)
+register_plugin('PublishedFile', sde.SGPublishedFile)
+register_plugin('HumanUser', sde.SGHumanUser)
